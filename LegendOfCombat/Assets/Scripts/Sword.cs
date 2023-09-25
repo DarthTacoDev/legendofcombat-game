91a91c56df5201b1d723b02d296714af77fb6137
DarthTacoDev <cosperkids@gmail.com>
2023-09-24 18:53:13 -0700

    Add Unity Project

---
+++ LegendOfCombat/Assets/Scripts/Sword.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private Animator animator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        playerControls = new PlayerControls();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();    
+    }
+
+    private void Attack()
+    {
+        //fire sword animation
+        animator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg; //angle to rotate sword while moving around the mouse to get a nice offset
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
